5a18afbfb4aad39b793c65984553114453a332a8
Stévan Corre <stevancorre@protonmail.com>
2022-07-22 19:01:02 +0200

    [express] setup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log("Ok");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import { getEnv, log } from "./helpers";
+
+const port = Number.parseInt(getEnv("PORT"));
+
+const app = express();
+
+app.use(cors({ origin: "*" }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    log(`Server listening on port ${port}`);
+});
